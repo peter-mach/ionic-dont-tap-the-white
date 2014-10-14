03ea898fbca6ce8f2f7ab067173cf81d84b67883
Piotr Machowski <piotr@machowski.co>
2014-10-14 20:01:27 +0200

    game logic; scoreboard; game over; replay

--- DontTapTheWhite/www/src/game/game.mdl.js
+++ DontTapTheWhite/www/src/game/game.mdl.js
@@ -13,10 +13,61 @@ angular.module('dttw.game', [])
     });
 }])
 
-.controller('GameCtrl', ['$scope', '$state', function($scope, $state) {
+.controller('GameCtrl', ['$scope', '$state', '$ionicPopup', '$timeout', function($scope, $state, $ionicPopup, $timeout) {
+	// game config
 	$scope.speed = 1.05;
 	$scope.score = 0;
-	$scope.maxScore = 0;
+	$scope.showCountdown = true;
+	$scope.countdown = 'ready?';
+
+	$timeout(function(){
+		$scope.countdown = 'GO!!!';
+		$timeout(function () {
+			$scope.showCountdown = false;
+			$scope.$broadcast('game:start');
+		}, 500);
+	}, 1000);
+
+	function gotoMenu () {
+		$state.go('menu');
+	}
+
+	function restartGame () {
+		//reload current state
+		$state.go($state.current, {}, { reload: true });
+	}
+
+	$scope.addScore = function () {
+		$scope.score += 1;
+		$scope.$apply();
+	};
+
+	$scope.gameOver = function () {
+		$ionicPopup.alert({
+			title: 'your score',
+			template: '<span class="highscore">' + $scope.score + '</span>',
+			buttons: [{
+					text: 'again!',
+					type: 'again',
+					onTap: function(e) {
+						return 'again';
+					}
+				},{
+					text: 'menu',
+					type: 'button-default',
+					onTap: function(e) {
+						return 'menu';
+					}
+				},
+			]
+		}).then(function(response) {
+			if (response === 'again') {
+				restartGame();
+			} else {
+				gotoMenu();
+			}
+		});
+	};
 
 }])
 
--- DontTapTheWhite/www/src/game/gameboard/gameboard.dir.js
+++ DontTapTheWhite/www/src/game/gameboard/gameboard.dir.js
@@ -2,16 +2,12 @@ angular.module('dttw.game')
 
 .directive('gameboard', [ function() {
 	return {
-		scope: true, // true = child scope
-		restrict: 'E', // E = Element
+		scope: true,
+		restrict: 'E',
 		replace: true,
 		templateUrl: 'src/game/gameboard/gameboard.html',
 		controller: function($scope, $element, $attrs, $transclude) {
-			var now,
-				speed 	     = 1.05,
-				dt           = 0,
-				last         = window.performance.now(),
-				framerate    = 1000/60,
+			var framerate    = 1000/60, //60 fps
 				framerateID;
 
 			$scope.rows = [];
@@ -24,6 +20,12 @@ angular.module('dttw.game')
 				for (var i = 0; i < 6; i++) {
 					$scope.rows[i].y += $scope.speed;
 					if ($scope.rows[i].y > 100) {
+						if ($scope.rows[i].black >= 0) {
+							//game over
+							$scope.stopGame();
+							$scope.gameOver();
+						}
+
 						$scope.rows[i].y -= 150;
 						$scope.rows[i].black = getRandomInt(0,3);
 						$scope.setBlackTile(i, $scope.rows[i].black);
@@ -32,7 +34,14 @@ angular.module('dttw.game')
 				}
 			}
 
-			function reset () {
+			function destroy () {
+				clearInterval(framerateID);
+				$element.off('touchstart');
+			}
+
+			//$scope methods
+			$scope.reset = function () {
+				$scope.score = 0;
 				$scope.rows = [];
 				for (var i = 0; i < 6; i++) {
 					$scope.rows[i] = {
@@ -42,28 +51,26 @@ angular.module('dttw.game')
 					$scope.setBlackTile(i, $scope.rows[i].black);
 					$scope.setRowPosition(i, $scope.rows[i].y);
 				}
-			}
-
-			function destroy () {
-				clearInterval(framerateID);
-			}
-
-			//$scope methods
-			$scope.reset = reset;
+				$scope.rows[0].black = -1;
+				$scope.setBlackTile(0, $scope.rows[0].black);
+			};
 
 			$scope.startGame = function () {
-				reset();
 				//start the game loop
 				framerateID = setInterval(updatePosition, framerate);
 			};
 
-			//directive cleanup on scope destroy event
+			$scope.stopGame = function () {
+				clearInterval(framerateID);
+			};
+
+			//registering event listeners
 			$scope.$on('$destroy', destroy);
+			$scope.$on('game:start', $scope.startGame);
 
 		},
 		link: function($scope, element, attrs, gameCtrl) {
 			var rowElements = [];
-			console.log("element", element);
 
 			for (var i = 0; i < element[0].children.length; i++) {
 				rowElements[i] = element[0].children[i];
@@ -72,11 +79,14 @@ angular.module('dttw.game')
 			element.on('touchstart', function (event) {
 				if (event.target.className.indexOf('black') > -1) {
 					event.target.className = 'green';
-					console.log("score");
+					$scope.rows[parseInt(event.target.getAttribute('row-id'))].black = -1;
+					$scope.addScore();
 				}
 				if (event.target.className === '') {
 					event.target.className = 'red flash';
-					console.log("wrong");
+					element.off('touchstart');
+					$scope.stopGame();
+					$scope.gameOver();
 				}
 			});
 
@@ -90,10 +100,8 @@ angular.module('dttw.game')
 				}
 			};
 
-			//initial fields position
+			//initial gameboard layout
 			$scope.reset();
-			//start game animation
-			$scope.startGame();
 		}
 	};
 }]);
