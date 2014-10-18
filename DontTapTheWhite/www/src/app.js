af653dd0648a352b0a0e0cafa0e7d11749d4f54d
Piotr Machowski <piotr@machowski.co>
2014-10-18 15:25:28 +0200

    remove unnecessary keyboard configuration

--- DontTapTheWhite/www/src/app.js
+++ DontTapTheWhite/www/src/app.js
@@ -18,10 +18,4 @@ angular.module('dttw', [
   $urlRouterProvider.otherwise('/menu');
 }])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-  });
-});
+;
